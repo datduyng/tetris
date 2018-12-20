bb4b4bd0869e97de478b2cf3a75f0fe02730a1f8
datduyng <datnguyen5653@huskers.unl.edu>
2018-12-19 23:29:16 -0600

    seperated shape to an object. Fixed old shape painting problem.

--- src/Board.java
+++ src/Board.java
@@ -1,9 +1,14 @@
-import java.applet.*;
-import java.awt.*;
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.security.InvalidKeyException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Random;
 
 /**
  * 
@@ -15,51 +20,29 @@ import java.util.*;
 // for keyboard input
 public class Board extends Applet implements KeyListener, Runnable {
 
-	static ArrayList<int[][]> shapes = new ArrayList<int[][]>(7);
-	ArrayList<int[]> coordinate = new ArrayList<int[]>(); // 0: x. 1: y
+	boolean running = true;
+	final static int WIDTH = 600;
+	final static int HEIGHT = 800;
 
-	private boolean running = true;
-	private final static int WIDTH = 600;
-	private final static int HEIGHT = 800;
+	final static int numW = 30;
+	final static int numH = 40;
+	final static int pixelWidth = WIDTH / numW;
+	final static int pixelHeight = HEIGHT / numH;
 
-	private static int numW = 30;
-	private static int numH = 40;
-	private int pixelWidth = WIDTH / numW;
-	private int pixelHeight = HEIGHT / numH;
+	static int M[][] = new int[numH][numW];
+	
+	Shape currShape; 
+	Random rand = new Random();
 
-	private static int M[][] = new int[numH][numW];
-
-	private int dx = 0;
-	private int dy = 1;
-	private int idx_currShape;
 	LinkedHashMap<Integer, ArrayList<Integer>> setToDraw = new LinkedHashMap<Integer, ArrayList<Integer>>();
 
-	public void initShape() {
-		int[][] square = new int[][] { { 1, 1 }, { 1, 1 } };
-		shapes.add(square);
-
-	}
-
-	public void generateShape() {
-		Random r = new Random();
-		this.idx_currShape = r.nextInt(7);
-		int[][] currShape = shapes.get(0);
-		for (int y = 0; y < currShape.length; y++) {
-			for (int x = 0; x < currShape[0].length; x++) {
-				// Set the offset according to board size
-				int[] tmp = new int[] { x + 14, y };
-				coordinate.add(tmp);
-			}
-		}
-	}
 
 	@Override
 	// Call first by the browser
 	public void init() {
-		initShape();
-		generateShape();
 		this.setBackground(Color.WHITE);
 		addKeyListener(this);
+		currShape = new Shape(rand.nextInt(Shape.shapes.length)); 
 	}
 
 	@Override
@@ -75,7 +58,6 @@ public class Board extends Applet implements KeyListener, Runnable {
 					try {
 						update();
 						repaint();
-						System.out.println("Running..");
 						Thread.sleep(200);
 					} catch (InterruptedException e) {
 						System.out.println("Interupt ERROR");
@@ -103,20 +85,17 @@ public class Board extends Applet implements KeyListener, Runnable {
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-		System.out.println("Painting..");
 		this.setSize(WIDTH, HEIGHT);
 		g2d.setColor(Color.RED);
 		g2d.drawString("Tetris game", 60, 20);
 		showStatus("Testing...");
-		for (int[] coor : coordinate) {
-			if (M[coor[1]][coor[0]] == 1) {
+		for (int[] coor : currShape.coordinate) {
 				g2d.fillRect(pixelWidth * coor[0], pixelHeight * coor[1], 20, 20);
-			}
 		}
+		
 		
 		// Draw previous blocks
 		for (Integer key : setToDraw.keySet()) {
-			System.out.println(key);
 			ArrayList<Integer> row = setToDraw.get(key);
 			for (Integer point : row) {
 				g2d.fillRect(pixelWidth * point, pixelHeight * key, 20, 20);		
@@ -126,46 +105,51 @@ public class Board extends Applet implements KeyListener, Runnable {
 
 	}
 
+	public void genShape() {
+		//rand
+		//delete old/free old 
+		currShape = new Shape(rand.nextInt(Shape.shapes.length));
+	}
 	public void update() {
-		// Graphics2D g2d = (Graphics2D) g;
-		//
 		// delete old coor before moving
 		// paint(g); //keep what was there before
-		for (int[] coor : coordinate) {
+		for (int[] coor : currShape.coordinate) {
 			M[coor[1]][coor[0]] = 0;
 		}
 		
 		// Check if the block is movable or not
-		for (int[] coor : coordinate) {
+		for (int[] coor : currShape.coordinate) {
+			if (coor[0] == 0 || coor[0] > numW - 2) {
+				currShape.dx = 0;
+			}
 			if (coor[1] >= numH - 1) {
-				dy = 0;
-				dx = 0;
+				currShape.dy = 0;
+				currShape.dx = 0;
 				updateBoard();
-				coordinate.clear();
-				generateShape();
+				currShape.coordinate.clear();
+				genShape();
 				break;
-			} if (coor[0] == 0 || coor[0] > numW - 2) {
-				dx = 0;
-			} if (M[coor[1] + dy][coor[0]] == 1) {
-				dx = 0;
-				dy = 0;
+			}
+			if (M[coor[1] + 1][coor[0]] == 1) {
+				currShape.dy = 0;
+				currShape.dx = 0;
 				updateBoard();
-				coordinate.clear();
-				generateShape();
+				currShape.coordinate.clear();
+				genShape();
 				break;
 			}
 		}
 		// Update moving box
-		for (int[] coor : coordinate) {
-			coor[1] += dy;
-			coor[0] += dx;
+		for (int[] coor : currShape.coordinate) {
+			//prevent falling to fast 
+			if(coor[1]+currShape.dy<=numH) if(M[coor[1]+currShape.dy][coor[0]] == 1) currShape.dy = 1;
+			coor[1] += currShape.dy;
+			coor[0] += currShape.dx;
 		}
 		// Change to rand idx once all shape got set up
-		for (int[] coor : coordinate) {
+		for (int[] coor : currShape.coordinate) {
 			M[coor[1]][coor[0]] = 1;
 		}
-		repaint();
-
 	}
 	
 	/*
@@ -173,10 +157,11 @@ public class Board extends Applet implements KeyListener, Runnable {
 	 */
 	
 	public void updateBoard() {
-		for (int[] coorToDraw : coordinate) {
+		for (int[] coorToDraw : currShape.coordinate) {
 			M[coorToDraw[1]][coorToDraw[0]] = 1;// Update matrix board
 			if (setToDraw.get(coorToDraw[1]) == null) {
 				ArrayList<Integer> val = new ArrayList<Integer>();
+				val.add(coorToDraw[0]);
 				setToDraw.put(coorToDraw[1], val);
 			} else {
 				setToDraw.get(coorToDraw[1]).add(coorToDraw[0]);
@@ -187,28 +172,25 @@ public class Board extends Applet implements KeyListener, Runnable {
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		System.out.println("Starting keyListen");
 		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-			dx = -1;
-			dy = 1;
-			System.out.println("Going left");
+			currShape.dx = -1;
+			currShape.dy = 1;
 		} else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-			dx = 1;
-			dy = 1;
-			System.out.println("Going Right");
+			currShape.dx = 1;
+			currShape.dy = 1;
 		} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-			dx = 0;
-			dy = 3;
+			currShape.dx = 0;
+			currShape.dy = 3;
 		} else {
-			dy = 1;
-			dx = 0;
+			currShape.dy = 1;
+			currShape.dx = 0;
 		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent arg0) {
-		dx = 0;
-		dy = 1;
+		currShape.dx = 0;
+		currShape.dy = 1;
 
 	}
 
