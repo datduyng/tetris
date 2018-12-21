e9102f55ff81b2cdb5fb023789e427a290bf3595
datduyng <datnguyen5653@huskers.unl.edu>
2018-12-21 11:23:27 -0600

    Fixed rotation of most all shape except 1. fixed freezing shape after rotate problem.

--- src/Debug.java
+++ src/Debug.java
@@ -2,10 +2,15 @@
 public class Debug {
 	public static void main (String args[]) {
 		Shape s = new Shape(3);
-		s.coordinate.add(new int [] {2, 3, 0});
-		s.coordinate.add(new int [] {2, 4, 1});
-		s.coordinate.add(new int [] {3, 4, 0});
-		s.coordinate.add(new int [] {3, 5, 0});
+		s.coordinate.add(new int [] {8, 1, 0});
+		s.coordinate.add(new int [] {8, 2, 1});
+		s.coordinate.add(new int [] {8, 3, 0});
+		s.coordinate.add(new int [] {8, 4, 0});
+		
+		System.out.println("Before rotate...");
+		for (int c [] : s.coordinate) {
+			System.out.println(c[0] + " " +  c[1]);
+		}
 		s.rotate();
 		System.out.println("Debugging...");
 		for (int c [] : s.coordinate) {
--- src/Shape.java
+++ src/Shape.java
@@ -7,17 +7,30 @@ public class Shape {
 	int dx, dy;
 	ArrayList<int[]> coordinate = new ArrayList<int[]>(); // 0: x. 1: y 2: COM
 
-	final static int[][][] shapes = { { { 0, 1 }, { 0, 1 }, { 1, 2 } },
-
-			{ { 1, 1 }, { 1, 1 } },
-
-			{ { 1, 0 }, { 1, 0 }, { 2, 1 } },
-
-			{ { 1, 0 }, { 1, 2 }, { 0, 1 } },
-
-			{ { 0, 1, 0 }, { 1, 2, 1 } },
-
-			{ { 1 }, { 2 }, { 1 }, { 1 } } };
+	final static int[][][] shapes =  {
+				  { { 0, 1 }, 
+			        { 0, 1 },
+			        { 1, 2 }},
+					 
+				  { { 1, 1 }, 
+				    { 1, 1 } },
+				      
+			      { { 1, 0 }, 
+				    { 1, 0 },
+				    { 2, 1 } },
+			      
+			      { { 1, 0 }, 
+					{ 1, 2 },
+					{ 0, 1 } },
+			      
+			      { { 0, 1, 0 }, 
+				    { 1, 2, 1 } },
+					
+			      { { 1 }, 
+					{ 2 },
+			      	{ 1 }, 
+					{ 1 }}
+		};
 
 	// there should be 7 shape
 	// ArrayList<int[][]> shapes = new ArrayList<int[][]>();
@@ -47,33 +60,32 @@ public class Shape {
 	}
 
 	/*
-	 * Function to rotate a block based on it's current coordinate
-	 */
+	 * Algorithm.
+	 * Matrix rotation formula : Mat dot product. 
+	 * 		  -		
+	 * [x'] = [cos(θ)  -sin(θ)][x]
+	 * [y']   [sin(θ)   cos(θ)][y]  
+	 */       
 
 	public void rotate() {
-		int originX = 0, originY = 0;
-		for (int[] coor : coordinate) {
-			if (coor[2] == 1) {
-				// Get the COM of the block
-				originX = coor[0];
-				originY = coor[1];
+		int COMx = 0, COMy = 0;
+		//find Center of mass coordinate x and y
+		for(int[] coor : this.coordinate) 
+			if(coor[2] == 1) {
+				COMx = coor[0];
+				COMy = coor[1];
+				break;
 			}
-		}
-		for (int[] c : coordinate) {
-			c[0] -= originX;
-			c[1] -= originY;
-		}
 		
-		for (int[] c : coordinate) {
-			int tmp = c[0];
-			c[0] = c[1];
-			c[1] = tmp;
-		}
+		if(COMx == 0 && COMy == 0) return; // If there are no COM on object. then obj is symetric
 		
-		for (int[] c : coordinate) {
-			c[0] += originY;
-			c[1] += originX;
-		}
-	}
+		for(int[] coor: this.coordinate) {
+			int shiftedx = coor[0] - COMx; 
+			int shiftedy = coor[1] - COMy; 
+			coor[0] = (int) (shiftedx*Math.cos(Math.toRadians(90.0)) - shiftedy*Math.sin(Math.toRadians(90.0))) + COMx;
+			coor[1] = (int) (shiftedx*Math.sin(Math.toRadians(90.0)) - shiftedy*Math.cos(Math.toRadians(90.0))) + COMy;
+			System.out.println("x:"+coor[0]+"|y:"+coor[1]);
+		}// end for
+	}// end rotate
 
 }
